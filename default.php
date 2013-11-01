49035d03980853336f17ddd581d137ce95e63565
Brett Goldman <brettg@axosoft.com>
2013-11-01 09:02:31 -0700

    test with сега на кирилица [otf: 5]

--- default.php
+++ default.php
@@ -13,7 +13,7 @@
                 $this->education=$education;
               $this->age=$age; //added not from original
            }
-           // Getter/Setter functions for all properties in the class test
+           // Getter/Setter functions for all properties in the class 
            function get_name() {
                 return $this->name;
            }
